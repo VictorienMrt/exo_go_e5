3d4ebc2d4ec828eab18495a996189f06a3d3c6b2
paul verny <victorien.moyret@gmail.com>
2023-11-16 13:13:28 +0100

    Add: Comments

--- main.go
+++ main.go
@@ -1,25 +1,30 @@
+// main package definition
 package main
 
+// Importing packages
 import (
-	"bufio"
-	"exo_go_e5/dictionary" // assurez-vous que ce chemin correspond au nom du module dans go.mod
-	"fmt"
-	"os"
-	"strings"
+	"bufio" // For reading input
+	"exo_go_e5/dictionary"
+	"fmt"     // For formatted I/O operations
+	"os"      // For interfacing with the operating system
+	"strings" // For string manipulation
 )
 
+// Function main
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	d := dictionary.New()
+	reader := bufio.NewReader(os.Stdin) // Create a new reader for standard input
+	d := dictionary.New()               // Init a new dictionary object
 
+	// Infinite loop to handle user input
 	for {
 		fmt.Println("\nSelect an action [add, define, remove, list, exit]:")
-		action, _ := reader.ReadString('\n')
+		action, _ := reader.ReadString('\n') // Action choice from user
 		action = strings.TrimSpace(action)
 
+		// Switch statement to handle the selected action
 		switch action {
 		case "add":
-			actionAdd(d, reader)
+			actionAdd(d, reader) // Calling the corresponding function
 		case "define":
 			actionDefine(d, reader)
 		case "remove":
@@ -30,11 +35,12 @@ func main() {
 			fmt.Println("End of program")
 			return
 		default:
-			fmt.Println("Not recognized.")
+			fmt.Println("Not recognized.") // Print error
 		}
 	}
 }
 
+// Function to add a word to the dictionary
 func actionAdd(d *dictionary.Dictionary, reader *bufio.Reader) {
 	fmt.Print("Enter a word: ")
 	word, _ := reader.ReadString('\n')
@@ -44,14 +50,15 @@ func actionAdd(d *dictionary.Dictionary, reader *bufio.Reader) {
 	definition, _ := reader.ReadString('\n')
 	definition = strings.TrimSpace(definition)
 
-	err := d.Add(word, definition)
+	err := d.Add(word, definition) // Add the word to the dictionary
 	if err != nil {
-		fmt.Println("Failed to add word:", err)
+		fmt.Println("Failed to add word:", err) // Print error
 	} else {
-		fmt.Println("Word added.")
+		fmt.Println("Word added.") // Print success message
 	}
 }
 
+// Function to define a word in the dictionary
 func actionDefine(d *dictionary.Dictionary, reader *bufio.Reader) {
 	fmt.Print("Enter a word: ")
 	word, _ := reader.ReadString('\n')
@@ -65,12 +72,13 @@ func actionDefine(d *dictionary.Dictionary, reader *bufio.Reader) {
 	}
 }
 
+// Function to remove a word from the dictionary
 func actionRemove(d *dictionary.Dictionary, reader *bufio.Reader) {
 	fmt.Print("Enter a word to remove: ")
 	word, _ := reader.ReadString('\n')
 	word = strings.TrimSpace(word)
 
-	err := d.Remove(word)
+	err := d.Remove(word) // Remove the word from the dictionary
 	if err != nil {
 		fmt.Println("Failed to remove word:", err)
 	} else {
@@ -78,10 +86,11 @@ func actionRemove(d *dictionary.Dictionary, reader *bufio.Reader) {
 	}
 }
 
+// Function to list all words and their definitions in the dictionary
 func actionList(d *dictionary.Dictionary) {
-	words, entries := d.List()
+	words, entries := d.List() // Get list of words and their corresponding entries
 	for _, word := range words {
-		entry := entries[word]
+		entry := entries[word] // Get each entry
 		fmt.Println(word, ":", entry)
 	}
 }
